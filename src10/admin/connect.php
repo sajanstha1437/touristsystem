f832040a103e4ae60ea719ae5324f2df9d47c065
Sajan Shrestha <49780480+sajanstha1437@users.noreply.github.com>
2019-08-23 13:55:21 +0900

    Create connect.php

---
+++ src10/admin/connect.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+<?php
+$conn =null;
+try{
+	$option =array(
+	PDO::ATTR_ERRMODE
+	=>PDO::ERRMODE_EXCEPTION);
+	$conn=new PDO(
+	"mysql:host=localhost;dbname=test;charset=utf8;",
+	"root","",$option);
+}
+catch (PDOException $e){
+	die($e->getmessage());
+}
+?>
+</body>
+</html>
