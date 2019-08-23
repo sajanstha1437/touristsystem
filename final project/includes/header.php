03e1777ee824ec526c96b7a20723b942ed3d146e
Sajan Shrestha <49780480+sajanstha1437@users.noreply.github.com>
2019-08-23 14:28:10 +0900

    Add files via upload

---
+++ final project/includes/header.php
@@ -0,0 +1,21 @@
+<head>
+<link href="../css/gallery.css" rel="stylesheet" type="text/css" />
+
+</head>
+	<div id=bannerdiv>
+    
+    </div>
+	<div id=menudiv>
+		<table align="right">
+			<tr>
+				<td ><a href="index.html">Home</a></td>
+				<td ><a href="intro.html">Introduction</a></td>
+				<td><a href="aboutus.html">About Us</a></td>
+				<td><a href="gallery.html">Gallery</a></td>
+				<td><a href="bookings.html">Reservation</a></td>
+				 <td><a href="issues.php">issues</a>				
+					</td>
+				
+			</tr>
+		</table>
+	</div>
